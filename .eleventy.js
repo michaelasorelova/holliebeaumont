dd03641e1ef7518e58dc1b8eaf919fc51067dc99
Michaela Šorelová <michaelasorelova@gmail.com>
2022-12-12 20:42:29 +0100

    fourth commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,13 @@
 module.exports = function(eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy("images");
-
     eleventyConfig.addPassthroughCopy("css");
+
+    eleventyConfig.addPassthroughCopy("script.js");
 
     return {
 
         templateFormats: ["njk", "html", "md"],
         
-        markdownTemplateEngine: "njk",
         htmlTemplateEngine: "njk",
         dataTemplateEngine: "njk", 
     }
