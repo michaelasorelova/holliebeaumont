6e96ce78d39827c6929ab36f82e1fd215d79d720
Michaela Šorelová <michaelasorelova@gmail.com>
2023-01-07 22:55:40 +0100

    first commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
-window.addEventListener("load", () => {
-    const menu = document.querySelector(".nav");
-    const toggle = document.querySelector(".hamburger");
+window.addEventListener('load', () => {
+    const menu = document.querySelector('.nav__menu');
+    const toggle = document.querySelector('.nav__toggle');
+    const body = document.querySelector('body');
 
-    toggle.addEventListener("click", () => {
-        menu.classList.toggle("active");
+    toggle.addEventListener('click', () => {
+        menu.classList.toggle('active');
+        body.classList.toggle('no-scroll');
+        toggle.classList.toggle('uil-multiply');
+        toggle.classList.toggle('uil-bars');
     });
 });
